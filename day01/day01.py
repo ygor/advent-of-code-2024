1fed7dc0e170caef557c3ab2c1e6360339f15ff9
Ygor Geurts <ygor.geurts@gmail.com>
2024-12-05 23:14:35 +0100

    simplify

--- day01/day01.py
+++ day01/day01.py
@@ -2,7 +2,7 @@ with open('input.txt', 'r') as file:
     part1 = sum(
         abs(y - x)
         for x, y in zip(
-            *map(lambda xs: sorted(map(int, xs)), zip(*[line.strip().split() for line in file]))
+            *map(lambda xs: sorted(map(int, xs)), zip(*[line.split() for line in file]))
         )
     )
     print(part1)
