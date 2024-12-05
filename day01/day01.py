7c8bb5b5b92a9184252fea2e96bb172e7854c295
Ygor Geurts <ygor.geurts@gmail.com>
2024-12-05 23:11:13 +0100

    initial commit

---
+++ day01/day01.py
@@ -0,0 +1,8 @@
+with open('input.txt', 'r') as file:
+    part1 = sum(
+        abs(y - x)
+        for x, y in zip(
+            *map(lambda xs: sorted(map(int, xs)), zip(*[line.strip().split() for line in file]))
+        )
+    )
+    print(part1)
