82bdd661108efbe5018fecab956ecb6faa0816c3
Ygor Geurts <ygor.geurts@gmail.com>
2024-12-05 23:30:19 +0100

    day01 part2

--- day01/day01.py
+++ day01/day01.py
@@ -1,8 +1,8 @@
 with open('input.txt', 'r') as file:
-    part1 = sum(
-        abs(y - x)
-        for x, y in zip(
-            *map(lambda xs: sorted(map(int, xs)), zip(*[line.split() for line in file]))
-        )
-    )
+    [xs, ys] = zip(*[map(int, line.split()) for line in file])
+    
+    part1 = sum(abs(y - x) for x, y in zip(*[sorted(xs), sorted(ys)]))
     print(part1)
+    
+    part2 = sum(x * ys.count(x) for x in xs)
+    print(part2)    
