bb00f4186c3cdc75d11aa0fd017cde72e48313b5
Ygor Geurts <ygor.geurts@gmail.com>
2024-12-06 18:30:03 +0100

    day3

---
+++ day03/main.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+import re
+
+
+def run(memory):
+    mul_pattern = r"mul\((\d{1,3}),(\d{1,3})\)"
+    matches = re.findall(mul_pattern, memory)
+    return sum(int(a) * int(b) for a, b in matches)
+
+memory = Path("input.txt").read_text()
+print("Part 1:", run(memory))
+
+do_pattern = r"do\(\)"
+dont_pattern = r"don\'t\(\)"
+
+print("Part 2:", sum(run(re.split(dont_pattern, segment)[0]) for segment in re.split(do_pattern, memory)))
