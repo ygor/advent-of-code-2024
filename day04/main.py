690581e19859811d2460a594a374c91c485b3c77
Ygor Geurts <ygor.geurts@gmail.com>
2024-12-06 20:03:51 +0100

    day04 part2

--- day04/main.py
+++ day04/main.py
@@ -5,17 +5,14 @@ import re
 
 lines = Path("input.txt").read_text().splitlines()
 matrix: np.ndarray = np.array([list(line) for line in lines])
+rows, cols = matrix.shape
 
 
 def count_xmas(lines: List[List[str]]) -> int:
-    return sum(
-        "".join(line + line[::-1]).count("XMAS")
-        for line in lines
-    )
+    return sum("".join(line + line[::-1]).count("XMAS") for line in lines)
 
 
-def get_diagonals(matrix: np.ndarray) -> List[List[str]]:
-    rows, cols = matrix.shape
+def diagonals(matrix: np.ndarray) -> List[List[str]]:
     return [matrix.diagonal(d).tolist() for d in range(-(rows - 1), cols)]
 
 
@@ -23,6 +20,22 @@ print(
     "Part 1:",
     count_xmas(matrix.tolist())
     + count_xmas(matrix.T.tolist())
-    + count_xmas(get_diagonals(matrix))
-    + count_xmas(get_diagonals(np.fliplr(matrix))),
+    + count_xmas(diagonals(matrix))
+    + count_xmas(diagonals(np.fliplr(matrix))),
 )
+
+
+def is_mas(line: List[str]) -> bool:
+    return "".join(line) == "MAS" or "".join(line) == "SAM"
+
+
+padded = np.pad(matrix, pad_width=1, mode="constant", constant_values=0)
+
+count = sum(
+    is_mas([padded[x - 1, y - 1], padded[x, y], padded[x + 1, y + 1]])
+    and is_mas([padded[x - 1, y + 1], padded[x, y], padded[x + 1, y - 1]])
+    for x in range(1, rows)
+    for y in range(1, cols)
+)
+
+print("Part 2:", count)
