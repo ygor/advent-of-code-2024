a88f49a0725dd9ca5a57ad9854b2c8be3c79987d
Ygor Geurts <ygor.geurts@gmail.com>
2024-12-06 22:32:52 +0100

    day05 part2

--- day05/main.py
+++ day05/main.py
@@ -3,6 +3,7 @@ from functools import partial
 from typing import Dict, List
 from collections import defaultdict
 from functools import reduce
+from toolz import complement, compose
 
 sections = Path("input.txt").read_text().split("\n\n")
 
@@ -15,24 +16,40 @@ rules = dict(
         defaultdict(list),
     )
 )
-updates = [update.split(",") for update in sections[1].split("\n") if update.strip()]
+updates = [update.split(",") for update in sections[1].split("\n")]
 
 
 def is_valid(update, rules: Dict[str, List[str]]) -> bool:
     return all(
-        [
-            update[i] not in rules or all(x not in update[:i] for x in rules[update[i]])
-            for i in range(0, len(update))
-        ]
+        update[i] not in rules or all(x not in update[:i] for x in rules[update[i]])
+        for i in range(0, len(update))
     )
+
+
+def to_value(update: List[str]) -> int:
+    return int(update[len(update) // 2])
 
 
 print(
     "Part 1:",
-    sum(
-        map(
-            lambda update: int(update[len(update) // 2]),
-            filter(partial(is_valid, rules=rules), updates),
-        )
-    ),
+    sum(map(to_value, filter(partial(is_valid, rules=rules), updates))),
 )
+
+invalid_updates = filter(complement(partial(is_valid, rules=rules)), updates)
+
+
+def fix_update(update: List[str]) -> List[str]:
+    result = []
+    for page in update:
+        if page not in rules:
+            result.append(page)
+        else:
+            index = min(
+                (result.index(y) for y in rules[page] if y in result),
+                default=len(result),
+            )
+            result.insert(index, page)
+    return result
+
+
+print("Part 2:", sum(map(compose(to_value, fix_update), invalid_updates)))
