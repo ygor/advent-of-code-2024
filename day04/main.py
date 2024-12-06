441e26e9f558e9cc9f84e6507c97e6eecfa7d882
Ygor Geurts <ygor.geurts@gmail.com>
2024-12-06 19:39:54 +0100

    formatting and typing

--- day04/main.py
+++ day04/main.py
@@ -1,19 +1,20 @@
 import numpy as np
 from pathlib import Path
+from typing import List
 import re
 
 lines = Path("input.txt").read_text().splitlines()
-matrix = np.array([list(line) for line in lines])
+matrix: np.ndarray = np.array([list(line) for line in lines])
 
 
-def count_xmas(lines):
+def count_xmas(lines: List[List[str]]) -> int:
     return sum(
-        len(re.findall("xmas", "".join(line + line[::-1]), flags=re.IGNORECASE))
+        len(re.findall("XMAS", "".join(line + line[::-1])))
         for line in lines
     )
 
 
-def get_diagonals(matrix):
+def get_diagonals(matrix: np.ndarray) -> List[List[str]]:
     rows, cols = matrix.shape
     return [matrix.diagonal(d).tolist() for d in range(-(rows - 1), cols)]
 
