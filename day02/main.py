fe049f13a7334cbcff9e288eae48319ad4b39d65
Ygor Geurts <ygor.geurts@gmail.com>
2024-12-06 16:05:16 +0100

    day2

--- day02/main.py
+++ day02/main.py
@@ -1,10 +1,24 @@
 from itertools import pairwise
+from pathlib import Path
 
-with open("input.txt", "r") as file:
-    reports = [[int(x) for x in line.split()] for line in file]
-    valid_reports = [
-        all(0 < b - a <= 3 for a, b in pairwise(report))
-        or all(-3 <= b - a < 0 for a, b in pairwise(report))
+reports = [
+    [int(x) for x in line.split()]
+    for line in Path("input.txt").read_text().splitlines()
+]
+
+
+def is_safe(report):
+    return all(0 < b - a <= 3 for a, b in pairwise(report)) or all(
+        -3 <= b - a < 0 for a, b in pairwise(report)
+    )
+
+
+print("Part 1:", sum(is_safe(report) for report in reports))
+print(
+    "Part 2:",
+    sum(
+        is_safe(report)
+        or any(is_safe(report[:i] + report[i + 1 :]) for i in range(0, len(report)))
         for report in reports
-    ]
-    print(sum(valid_reports))
+    ),
+)
