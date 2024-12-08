7d2c593dac69136efebc1c39c8f674ed1fcd975b
Ygor Geurts <ygor.geurts@gmail.com>
2024-12-08 21:15:39 +0100

    formatting

--- day08/main.py
+++ day08/main.py
@@ -4,26 +4,27 @@ import itertools
 
 
 def load_map(filename):
-    return np.array([list(line.strip()) for line in Path(filename).read_text().splitlines()])
+    return np.array(
+        [list(line.strip()) for line in Path(filename).read_text().splitlines()]
+    )
 
 
-def get_antennas(map):
-    return {
-        freq: [tuple(coord) for coord in np.argwhere(map == freq)]
-        for freq in np.unique(map) if freq != "."
-    }
-    
+def antennas(map: np.ndarray) -> list[tuple[int, int]]:
+    return [
+        [tuple(coord) for coord in np.argwhere(map == freq)]
+        for freq in np.unique(map)
+        if freq != "."
+    ]
+
 
 def antinodes(map):
-    antennas = get_antennas(map)
-    nodes = set()
-    max_x, max_y = map.shape
-    
-    for coords in antennas.values():
-        for (x, y), (a, b) in itertools.combinations(coords, 2):
-            dx, dy = x - a, y - b
-            nodes.update({(x + dx, y + dy), (a - dx, b - dy)})
-    
-    return set(filter(lambda coord: 0 <= coord[0] < max_x and 0 <= coord[1] < max_y, nodes))
+    return {
+        tuple(pos)
+        for points in antennas(map)
+        for p1, p2 in itertools.combinations(np.array(list(points)), 2)
+        for pos in [p1 + (p1 - p2), p2 + (p2 - p1)]
+        if all(0 <= pos) and all(pos < map.shape)
+    }
+
 
 print("Part 1:", len(antinodes(load_map("input.txt"))))
