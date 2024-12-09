8806dba98e720ed14ef4c7d37d2b4588917e0fa5
Ygor Geurts <ygor.geurts@gmail.com>
2024-12-09 09:15:06 +0100

    day09 part1

--- day09/main.py
+++ day09/main.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import itertools
 
 
-def load_memory():
+def load_memory() -> list[str]:
     return list(
         itertools.chain.from_iterable(
             [index // 2] * int(value) if index % 2 == 0 else ["."] * int(value)
@@ -11,4 +11,23 @@ def load_memory():
     )
 
 
-print("Part 1:", load_memory())
+def optimise(memory: list[str]) -> list[str]:
+    empty = 0
+    for i in range(len(memory) - 1, -1, -1):
+        if memory[i] != ".":
+            while memory[empty] != "." and empty < i:
+                empty += 1
+            if empty < i:
+                memory[empty], memory[i] = memory[i], "."
+    return memory
+
+
+def checksum(memory: list[str]) -> int:
+    return sum(
+        int(value) * index
+        for value, index in zip(memory, itertools.count())
+        if value != "."
+    )
+
+
+print("Part 1:", checksum(optimise(load_memory())))
