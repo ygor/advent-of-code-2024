c396a5676226ef38ae88e892ac069d9eccc0d7ac
Ygor Geurts <ygor.geurts@gmail.com>
2024-12-09 11:04:21 +0100

    day09 part2

--- day09/main.py
+++ day09/main.py
@@ -22,6 +22,32 @@ def optimise(memory: list[str]) -> list[str]:
     return memory
 
 
+def load_memory_p2() -> list[tuple[str, int]]:
+    return list(
+        (index // 2, int(value)) if index % 2 == 0 else (".", int(value))
+        for index, value in enumerate(Path("input.txt").read_text())
+    )
+
+def optimise_p2(memory: list[tuple[str, int]]) -> list[str]:
+    empty = 0
+    index = len(memory) - 1
+    while index >= 0:
+        id, size = memory[index]
+        if id != ".":
+            empty = 0
+            while (memory[empty][0] != "." or memory[empty][1] < size) and empty < index:
+                empty += 1  
+            if empty < index:
+                space = memory[empty][1]
+                memory[empty] = (".", space - size)
+                memory[index] = (".", size)
+                memory.insert(empty, (id, size))
+                index += 1
+        index -= 1
+    
+    return list(itertools.chain.from_iterable([id] * size for id, size in memory if size > 0))
+
+
 def checksum(memory: list[str]) -> int:
     return sum(
         int(value) * index
@@ -31,3 +57,4 @@ def checksum(memory: list[str]) -> int:
 
 
 print("Part 1:", checksum(optimise(load_memory())))
+print("Part 2:", checksum(optimise_p2(load_memory_p2())))
