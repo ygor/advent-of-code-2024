81ac531dc4936294390ae9dfcd906e325f7a8393
Ygor Geurts <ygor.geurts@rtl.nl>
2024-12-10 10:01:27 +0100

    day10 part1 wip

--- day10/main.py
+++ day10/main.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import numpy as np
 
-DIRECTIONS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+DIRECTIONS = [np.array((1, 0)), np.array((-1, 0)), np.array((0, 1)), np.array((0, -1))]
 
 
 def load_heightmap(file_path: str) -> np.ndarray:
@@ -9,25 +9,37 @@ def load_heightmap(file_path: str) -> np.ndarray:
     return np.array([list(map(int, line)) for line in lines])
 
 
-def find_trailheads(heightmap: np.ndarray) -> list[tuple[int, int]]:
-    return list(map(tuple, np.argwhere(heightmap == 0)))
+def trailheads(heightmap: np.ndarray) -> list[np.array]:
+    return [np.array(pos) for pos in np.argwhere(heightmap == 0)]
 
-def explore(heightmap: np.ndarray, trail: list[tuple[int, int]]) -> list[list[tuple[int, int]]]:
+
+def explore(
+    heightmap: np.ndarray, trail: list[np.array]
+) -> list[list[np.array]]:
     results = []
     current_position = trail[-1]
-    if heightmap[current_position] == 9:
+
+    if heightmap[tuple(current_position)] == 9:
         results.append(trail)
     else:
-        current_height = heightmap[current_position]
         for direction in DIRECTIONS:
             next_position = current_position + direction
-            if heightmap[next_position] == current_height + 1:
+            if (
+                0 <= next_position[0] < heightmap.shape[0]
+                and 0 <= next_position[1] < heightmap.shape[1]
+                and heightmap[tuple(next_position)]
+                == heightmap[tuple(current_position)] + 1
+            ):
                 results.extend(explore(heightmap, trail + [next_position]))
     return results
 
-def find_trails(heightmap: np.ndarray) -> list[list[tuple[int, int]]]:
-    trailheads = find_trailheads(heightmap)
-    return [explore(heightmap, trailhead) for trailhead in trailheads]
+
+def find_trails(heightmap: np.ndarray) -> list[list[np.array]]:
+    result = 0
+    for trailhead in trailheads(heightmap):
+        trails = explore(heightmap, [trailhead])
+        result += len(trails)
+    return result
 
 
 print("Part 1:", find_trails(load_heightmap("input.txt")))
