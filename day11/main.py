6eccd3ed82af48c47a50d0dee57795fcb18d53cd
Ygor Geurts <ygor.geurts@rtl.nl>
2024-12-12 00:29:27 +0100

    formatting

--- day11/main.py
+++ day11/main.py
@@ -7,7 +7,7 @@ def stones() -> dict[int, int]:
     return dict(Counter(numbers))
 
 
-def evolve(stones: dict[int, int], count):
+def blink(stones: dict[int, int], count: int) -> int:
     for _ in range(count):
         new_stones = defaultdict(int)
         for value, freq in stones.items():
@@ -17,12 +17,12 @@ def evolve(stones: dict[int, int], count):
                 s = str(value)
                 mid = len(s) // 2
                 new_stones[int(s[:mid])] += freq
-                new_stones[int(s[mid:])] += freq                
+                new_stones[int(s[mid:])] += freq
             else:
                 new_stones[value * 2024] += freq
         stones = new_stones
     return sum(stones.values())
 
 
-print("Part 1:", evolve(stones(), 25))
-print("Part 2:", evolve(stones(), 75))
+print("Part 1:", blink(stones(), 25))
+print("Part 2:", blink(stones(), 75))
