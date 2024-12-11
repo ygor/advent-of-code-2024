e038561d87cbbef1cfecf351ccab847538804079
Ygor Geurts <ygor.geurts@rtl.nl>
2024-12-12 00:06:25 +0100

    formatting

--- day11/main.py
+++ day11/main.py
@@ -1,29 +1,29 @@
 from pathlib import Path
-import numpy as np
-from multiprocessing import Pool
+from math import floor, log10
 
 
 def stones() -> list[int]:
     return [int(stone) for stone in Path("input.txt").read_text().split()]
 
 
-def change(stone: int) -> list[int]:
-    match stone:
-        case 0:
-            return [1]
-        case s if len(str(s)) % 2 == 0:
-            s = str(s)
-            mid = len(s) // 2
-            return [int(s[:mid]), int(s[mid:])]
-        case s:
-            return [s * 2024]
+def change(stone: int, times: int) -> list[int]:
+    if times == 0:
+        return [stone]
+    if stone == 0:
+        return change(1, times - 1)
+
+    num_digits = floor(log10(stone)) + 1
+    if num_digits % 2 == 0:
+        mid = num_digits // 2
+        divisor = 10**mid
+        return change(stone // divisor, times - 1) + change(stone % divisor, times - 1)
+
+    return change(stone * 2024, times - 1)
 
 
 def blink(stones: list[int], times: int) -> list[int]:
-    with Pool() as pool:
-        for _ in range(times):
-            stones = pool.map(change, stones)
-            stones = [item for stone in stones for item in stone]
-    return stones
+    return [item for stone in stones for item in change(stone, times)]
 
-print("Part 1:", len(blink(stones(), 75)))
+
+print("Part 1:", len(blink(stones(), 25)))
+print("Part 2:", len(blink(stones(), 75)))
